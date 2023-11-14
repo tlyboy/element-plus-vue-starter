398524396431941aead1d21746ff91607d95f704
Guany <1084072690@qq.com>
2023-11-14 09:16:17 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import dayjs from 'dayjs'
-import 'dayjs/locale/zh-cn'
 
 const locale = ref(zhCn)
-
-dayjs.locale('zh-cn')
 </script>
 
 <template>
